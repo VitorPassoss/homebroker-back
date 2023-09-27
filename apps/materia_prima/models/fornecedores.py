4826f551ac61c2e80f5adf120ea717434cec5ca5
vitor <vitorpassosbrit@gmail.com>
2023-09-27 01:33:45 -0300

    pro

--- apps/materia_prima/models/fornecedores.py
+++ apps/materia_prima/models/fornecedores.py
@@ -7,5 +7,7 @@ class Fornecedores(models.Model):
         verbose_name="CPF/CNPJ",
         max_length=14,
         blank=True,
+        null=True
     )
     razao_social = models.CharField(max_length=255)
+    contato = models.CharField(max_length=255,  blank=True, null=True)
