2b2804a20849de6917cba50d0bc198a959543a50
vitor <vitorpassosbrit@gmail.com>
2023-11-13 02:02:08 -0300

    new date fre

--- apps/materia_prima/models/entrada.py
+++ apps/materia_prima/models/entrada.py
@@ -10,7 +10,7 @@ class Entrada(models.Model):
         max_digits=21, decimal_places=3, blank=True, null=True
     )
     valor = models.DecimalField(max_digits=19, decimal_places=2, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
 
 
     class Meta:
